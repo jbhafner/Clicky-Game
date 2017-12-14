e2bf6dbc99b265b133c24afaa52f6274269cdbd0
jbhafner <brian_hafner@yahoo.com>
2017-12-14 11:01:38 -0800

    working version

---
+++ src/components/GameArea/gameArea.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import './gameArea.css'
+import flags from "./flags.json"
+
+class GameArea extends Component {
+	state = {
+		score: 0,
+		guessed: [],
+    message: ""
+	}
+
+	handleClick(e) {
+		const clicked = e.target.dataset.id
+    if (this.state.guessed.indexOf(clicked) > -1) {
+      this.setState({
+        score: 0,
+        guessed: [],
+        message: "You lose!"
+      })
+    } else {
+      this.setState({
+        score: this.state.score + 1,
+        guessed: this.state.guessed.concat(clicked),
+        message: ""
+      })
+    }
+	}
+
+	render() {
+    let flagsArray = [].concat(flags)
+    flagsArray = shuffle(flagsArray, [])
+    console.log('flagsArray shuffle: ', flagsArray)
+		return (
+			<div className="game">
+        <h2>Score: {this.state.score} <span className="message">{this.state.message}</span></h2>
+				<div className="pics">
+          { flagsArray.map(flag => (
+              <Pic 
+                handleClick={this.handleClick.bind(this)}
+                key={flag.id}
+                link={flag.link}
+                id={flag.id} />
+            )
+          )}
+        </div>
+			</div>
+		)
+	}
+
+}
+
+const Pic = props => {
+  console.log('props:', props)
+	return (
+		<div className="pic">
+			<img 
+				src={ props.link }
+				onClick={props.handleClick}
+				alt={props.link}
+				data-id={props.id}
+				/>
+		</div>
+	);
+}
+
+function shuffle(arr1, arr2) {
+  if (arr1.length < 1) {
+    return arr2
+  } else {
+    let randomIndex = Math.floor(Math.random() * arr1.length)
+    let arr = arr1
+    console.log(randomIndex)
+    arr2.push(arr1[randomIndex])
+    arr.splice(randomIndex, 1)
+    return shuffle(arr, arr2)
+  }
+}
+
+export default GameArea
---
+++ src/components/Instructions/instructions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './instructions.css';
+
+const Instructions = () => {
+	return (
+		<div id="instructions">
+			<h2>Don't click the same flag twice!</h2>
+		</div>
+	);
+}
+
+export default Instructions;
